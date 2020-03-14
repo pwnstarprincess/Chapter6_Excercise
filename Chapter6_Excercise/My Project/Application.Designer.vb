dd26e3dc712002986ac0a3a4a18287db8154f28c
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-03-13 19:56:45 -0600

    initial commit

--- Chapter6_Excercise/My Project/Application.Designer.vb
+++ Chapter6_Excercise/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Chapter6_Excercise.Form1
+            Me.MainForm = Global.Chapter6_Excercise.ProjectileForm
         End Sub
     End Class
 End Namespace
