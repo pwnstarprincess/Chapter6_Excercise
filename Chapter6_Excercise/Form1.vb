7e0bd17c458fbdfde0e5803d88918e2d69084ff8
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-03-14 05:50:55 -0600

    add to input validation

--- Chapter6_Excercise/Form1.vb
+++ Chapter6_Excercise/Form1.vb
@@ -43,6 +43,12 @@ Public Class ProjectileForm
 
     End Sub
 
+    Private Sub ThrowError()
+
+        TotalOutputBox.Text = "ERROR!!! Please enter a numeric value for the initial height and initial velocity"
+
+    End Sub
+
     Private Function HeightAndVelocityNotEmpty() As Boolean
 
         If (Not String.IsNullOrEmpty(heightBox.Text) And Not String.IsNullOrEmpty(velocityBox.Text)) Then
@@ -50,6 +56,16 @@ Public Class ProjectileForm
         Else
             Return False
         End If
+    End Function
+
+    Private Function InputValid() As Boolean
+
+        If (Not String.IsNullOrEmpty(heightBox.Text) And Not String.IsNullOrEmpty(velocityBox.Text) AndAlso IsNumeric(heightBox.Text) And IsNumeric(velocityBox.Text)) Then
+            Return True
+        Else
+            Return False
+        End If
+
     End Function
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -64,12 +80,31 @@ Public Class ProjectileForm
 
     Private Sub MaxHeightButton_Click(sender As Object, e As EventArgs) Handles MaxHeightButton.Click
 
-        If (HeightAndVelocityNotEmpty()) Then
+        If (InputValid()) Then
             ClearOutputField()
             TotalOutputBox.AppendText("Max Height:    " + Convert.ToString(CalculateHeightAsFunctionOfTime(GetHeight(), GetVelocity(), (GetVelocity() / GRAVITY))) & Environment.NewLine)
             ClearInputFields()
+        Else
+            ThrowError()
         End If
 
 
     End Sub
+
+    Private Sub HeightBox_TextChanged(sender As Object, e As EventArgs) Handles heightBox.Leave
+
+        If (String.IsNullOrEmpty(heightBox.Text) Or Not IsNumeric(heightBox.Text)) Then
+            heightBox.Clear()
+            ThrowError()
+        End If
+    End Sub
+
+    Private Sub VelocityBox_TextChanged(sender As Object, e As EventArgs) Handles velocityBox.Leave
+
+        If (String.IsNullOrEmpty(velocityBox.Text) Or Not IsNumeric(velocityBox.Text)) Then
+            ThrowError()
+            velocityBox.Clear()
+        End If
+    End Sub
+
 End Class
