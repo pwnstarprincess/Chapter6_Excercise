98716eb11373af8fca5b396631d7c2d78d8a1e1b
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-03-21 20:17:33 -0600

    account for 0 height and 0 velocity

--- Chapter6_Excercise/Form1.vb
+++ Chapter6_Excercise/Form1.vb
@@ -24,11 +24,16 @@ Public Class ProjectileForm
 
     'Function to determine if the ball hit the ground
     Private Function BallHitGround(time As Double) As Boolean
-        'IF H(t) < or equal to 0 then the ball hit the ground
-        'Checking to see if time has exceeded 0 here as well because technically we can 
-        'Launch the projectile from the origin of the round at (0,0)
-        If (time > 0 And CalculateHeightAsFunctionOfTime(GetHeight(), GetVelocity(), time) <= 0) Then
+        'If the ball starts at the ground and is not projected up with any velocity then the ball never
+        'Leaves the ground
+        If GetHeight() = 0 And GetVelocity() = 0 Then
             Return True
+
+            'IF H(t) < or equal to 0 then the ball hit the ground
+            'Checking to see if time has exceeded 0 here as well because technically we can 
+            'Launch the projectile from the origin of the round at (0,0)
+        ElseIf (time > 0 And CalculateHeightAsFunctionOfTime(GetHeight(), GetVelocity(), time) <= 0) Then
+        Return True
         Else
             'h(t) greater than 0 return false because the ball has not hit the ground yet
             Return False
